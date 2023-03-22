375ee56470070d616f1be46cc732a36ca0782eaf
Pedrosheep00/mobileApps <24697@student.dorset_college.ie>
2023-03-22 14:51:17 +0000

    Home page with 4 buttons some text information and a copyright text.

--- app/src/main/java/com/example/mobileapps1/MainActivity.kt
+++ app/src/main/java/com/example/mobileapps1/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mobileapps1
 
 import android.app.ApplicationExitInfo
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +19,18 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
-        val education_button = findViewById<Button>(R.id.education_btn)
-        education_button.setOnClickListener{}
+//        val education_button = findViewById<Button>(R.id.education_btn)
+//        education_button.setOnClickListener{
+//        }
+
+        education_btn.setOnClickListener{
+            Intent(this, education::class.java).also {
+            startActivity(it)
+            }
+        }
+
+
+
+
     }}
 
